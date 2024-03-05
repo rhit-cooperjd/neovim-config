8ec3211de7cb62870e0dcfc3364dc946ad26dfa8
rhit-cooperjd <cooperjd@rose-hulman.edu>
2024-03-05 09:11:03 -0500

    change nvim binds

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -2,4 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("nmap > v>")
+vim.cmd("nmap < v<")
+vim.cmd("set number")
+vim.cmd("imap jk <Esc>")
 vim.g.mapleader = " "
